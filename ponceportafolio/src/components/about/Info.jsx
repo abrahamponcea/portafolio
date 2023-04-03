a0d8ce5088b25a94d847086bcb053e82d458ca70
abrahamponcea <83612691+abrahamponcea@users.noreply.github.com>
2023-04-03 17:17:46 -0600

    2 secciones
    Se agrego la sección home y la sección about me

---
+++ ponceportafolio/src/components/about/Info.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='about__info grid'>
+        <div className='about__box'>
+            <i class='bx bxs-building-house about__icon'></i>
+            <h3 className='about__title'>University of Guanajuato</h3>
+            <span className='about__subtitle'>Bachelor of Computer Systems Engineering</span>
+        </div>
+
+        <div className='about__box'>
+            <i class='bx bx-award about__icon'></i>
+            <h3 className='about__title'>Experience</h3>
+            <span className='about__subtitle'>2 Years Woking</span>
+        </div>
+        
+        <div className='about__box'>
+            <i class='bx bx-briefcase-alt about__icon'></i>
+            <h3 className='about__title'>Completed</h3>
+            <span className='about__subtitle'>4 + Projects</span>
+        </div>
+
+        <div className='about__box'>
+            <i class='bx bx-headphone about__icon'></i>
+            <h3 className='about__title'>Support</h3>
+            <span className='about__subtitle'>Online 24/7</span>
+        </div>
+
+    </div>
+  )
+}
+
+export default Info
